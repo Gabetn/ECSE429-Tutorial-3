f690ffe163bea151107f39fbbbdf10c0ff0350fd
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-09-24 19:01:16 -0400

    updated imports

--- src/test/java/ca/mcgill/ecse429/AppTest.java
+++ src/test/java/ca/mcgill/ecse429/AppTest.java
@@ -1,8 +1,8 @@
 package ca.mcgill.ecse429;
 
-import junit.framework.Test;
+/*import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.TestSuite;*/
 import org.junit.Test;
 
 /**
